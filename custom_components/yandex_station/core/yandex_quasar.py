b28e2725cb15acb65f0435a998a83dca7e8ae85c
Alexey Khit <alexey.khit@gmail.com>
2021-03-18 11:17:47 +0300

    Fix again new Yandex API #138

--- custom_components/yandex_station/core/yandex_quasar.py
+++ custom_components/yandex_station/core/yandex_quasar.py
@@ -117,10 +117,14 @@ class YandexQuasar:
 
     async def add_scenario(self, device_id: str):
         """Добавляет сценарий-пустышку."""
+        name = encode(device_id)
         payload = {
-            'name': encode(device_id),
+            'name': name,
             'icon': 'home',
-            'trigger_type': 'scenario.trigger.voice',
+            'triggers': [{
+                'type': 'scenario.trigger.voice',
+                'value': name[3:]
+            }],
             'requested_speaker_capabilities': [],
             'devices': [{
                 'id': device_id,
