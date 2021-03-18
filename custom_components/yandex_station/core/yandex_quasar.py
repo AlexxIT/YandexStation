e2dfaf58fe30583dfadaaeac8a5989735c200812
Alexey Khit <alexey.khit@gmail.com>
2021-03-18 09:49:49 +0300

    Fix new Yandex API #138

--- custom_components/yandex_station/core/yandex_quasar.py
+++ custom_components/yandex_station/core/yandex_quasar.py
@@ -183,10 +183,14 @@ class YandexQuasar:
         _LOGGER.debug(f"{device['name']} => cloud | {text}")
 
         action = 'phrase_action' if is_tts else 'text_action'
+        name = encode(device_id)
         payload = {
-            'name': encode(device_id),
+            'name': name,
             'icon': 'home',
-            'trigger_type': 'scenario.trigger.voice',
+            'triggers': [{
+                'type': 'scenario.trigger.voice',
+                'value': name[3:]
+            }],
             'requested_speaker_capabilities': [],
             'devices': [{
                 'id': device_id,
