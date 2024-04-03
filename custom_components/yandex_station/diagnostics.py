586a23334ecc632a1f80a680b1a07d0a97d873bb
Alex X <alexey.khit@gmail.com>
2024-04-03 16:25:00 +0300

    Add include section to diagnostics

--- custom_components/yandex_station/diagnostics.py
+++ custom_components/yandex_station/diagnostics.py
@@ -4,33 +4,48 @@ from homeassistant.helpers.device_registry import DeviceEntry
 
 from .core.const import DOMAIN
 from .core.yandex_quasar import YandexQuasar
+from .hass import hass_utils
 
 
-async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry):
-    quasar: YandexQuasar = hass.data[DOMAIN][entry.unique_id]
-    return {"devices": quasar.devices, "errors": get_errors(hass)}
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+):
+    quasar: YandexQuasar = hass.data[DOMAIN][config_entry.unique_id]
+
+    info = get_diagnostics(hass, config_entry)
+    info["device"] = quasar.devices
+    return info
 
 
 async def async_get_device_diagnostics(
-    hass: HomeAssistant, entry: ConfigEntry, device: DeviceEntry
+    hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
 ):
     did = next(did for _, did in device.identifiers)
 
-    quasar: YandexQuasar = hass.data[DOMAIN][entry.unique_id]
+    quasar: YandexQuasar = hass.data[DOMAIN][config_entry.unique_id]
     device = next(
         device
         for device in quasar.devices
         if device["id"] == did or device.get("quasar_info", {}).get("device_id") == did
     )
-    return {"device": device, "errors": get_errors(hass)}
+
+    info = get_diagnostics(hass, config_entry)
+    info["device"] = device
+    return info
 
 
-def get_errors(hass: HomeAssistant) -> list | str:
+def get_diagnostics(hass: HomeAssistant, config_entry: ConfigEntry) -> dict:
     try:
-        return [
+        errors = [
             entry.to_dict()
             for key, entry in hass.data["system_log"].records.items()
             if DOMAIN in str(key)
         ]
     except Exception as e:
-        return repr(e)
+        errors = repr(e)
+
+    include = [
+        device["id"] for _, device, _ in hass_utils.incluce_devices(hass, config_entry)
+    ]
+
+    return {"errors": errors, "include": include}
