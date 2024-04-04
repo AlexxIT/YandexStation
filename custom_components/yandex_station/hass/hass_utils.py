9ca54ba1b3721e900cd42362948ec3f778cf75c8
Alex X <alexey.khit@gmail.com>
2024-04-04 07:16:53 +0300

    Add fake devices support (for testing)

--- custom_components/yandex_station/hass/hass_utils.py
+++ custom_components/yandex_station/hass/hass_utils.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_INCLUDE
 from homeassistant.core import HomeAssistant
@@ -29,3 +32,10 @@ def incluce_devices(
                     break
 
     return devices
+
+
+def load_fake_devies(hass: HomeAssistant, quasar: YandexQuasar):
+    path = hass.config.path(DOMAIN + ".json")
+    if os.path.isfile(path):
+        with open(path, "rb") as f:
+            quasar.devices += json.load(f)
