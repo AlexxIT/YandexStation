a6bb37c47fed915f25cd532b77b900c94cbbc586
Alex X <alexey.khit@gmail.com>
2024-04-05 07:55:24 +0300

    Update tests

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -103,34 +103,3 @@ def test_socket_yandex():
             }
         },
     }
-
-
-def test_washing_machine():
-    device = {
-        "id": "xxx",
-        "name": "Стиральная машина",
-        "names": ["Стиральная машина"],
-        "type": "devices.types.washing_machine",
-        "icon_url": "https://avatars.mds.yandex.net/get-iot/icons-devices-devices.types.washing_machine.svg/orig",
-        "state": "offline",
-        "groups": [],
-        "room": "Ванная",
-        "capabilities": [
-            {
-                "retrievable": true,
-                "type": "devices.capabilities.on_off",
-                "state": {"instance": "on", "value": true},
-                "parameters": {"split": false},
-            },
-            {
-                "retrievable": true,
-                "type": "devices.capabilities.toggle",
-                "state": {"instance": "pause", "value": true},
-                "parameters": {"instance": "pause", "name": "пауза"},
-            },
-        ],
-        "properties": [],
-        "skill_id": "xxx",
-        "external_id": "xxx",
-        "favorite": false,
-    }
