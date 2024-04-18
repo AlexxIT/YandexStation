9c9ce4fd7f6d548cd1781d1798219fda42eae9c5
Alex X <alexey.khit@gmail.com>
2024-04-18 16:16:25 +0300

    Update imports for backward tests

--- tests/test_backward.py
+++ tests/test_backward.py
@@ -1,26 +1,24 @@
 from homeassistant.const import REQUIRED_PYTHON_VER
 
 from custom_components.yandex_station import *
-from custom_components.yandex_station.button import YandexCustomButton
-from custom_components.yandex_station.camera import YandexLyrics
-from custom_components.yandex_station.climate import YandexClimate
-from custom_components.yandex_station.config_flow import YandexStationFlowHandler
-from custom_components.yandex_station.cover import YandexCover
-from custom_components.yandex_station.diagnostics import (
-    async_get_config_entry_diagnostics,
-)
-from custom_components.yandex_station.humidifier import YandexHumidifier
-from custom_components.yandex_station.intent import async_setup_intents
-from custom_components.yandex_station.light import YandexLight
-from custom_components.yandex_station.media_player import YandexIntents
-from custom_components.yandex_station.notify import YandexStationNotificationService
-from custom_components.yandex_station.number import YandexCustomNumber
-from custom_components.yandex_station.remote import YandexOther
-from custom_components.yandex_station.select import YandexEqualizer
-from custom_components.yandex_station.sensor import YandexCustomSensor
-from custom_components.yandex_station.switch import YandexSwitch
-from custom_components.yandex_station.vacuum import YandexVacuum
-from custom_components.yandex_station.water_heater import YandexKettle
+from custom_components.yandex_station.button import *
+from custom_components.yandex_station.camera import *
+from custom_components.yandex_station.climate import *
+from custom_components.yandex_station.config_flow import *
+from custom_components.yandex_station.cover import *
+from custom_components.yandex_station.diagnostics import *
+from custom_components.yandex_station.humidifier import *
+from custom_components.yandex_station.intent import *
+from custom_components.yandex_station.light import *
+from custom_components.yandex_station.media_player import *
+from custom_components.yandex_station.notify import *
+from custom_components.yandex_station.number import *
+from custom_components.yandex_station.remote import *
+from custom_components.yandex_station.select import *
+from custom_components.yandex_station.sensor import *
+from custom_components.yandex_station.switch import *
+from custom_components.yandex_station.vacuum import *
+from custom_components.yandex_station.water_heater import *
 
 
 def test_backward():
