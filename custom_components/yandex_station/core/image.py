606225827ef9bd47bf3607003afe20b8cf2b4750
Alex X <alexey.khit@gmail.com>
2025-01-25 14:17:46 +0300

    Fix drawing of lyrics #590

--- custom_components/yandex_station/core/image.py
+++ custom_components/yandex_station/core/image.py
@@ -6,7 +6,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 WIDTH = 1280
 HEIGHT = 720
-HEIGHT2 = 720 // 2
+HEIGHT2 = HEIGHT // 2
+HEIGHT4 = HEIGHT // 4
 
 
 def font_path() -> str:
@@ -20,11 +21,13 @@ def draw_text(
     box: tuple,
     anchor: str,
     fill: str | tuple,
-    font: ImageFont,
+    font_size: int,
     line_width: int = 20,
 ):
     """Draw multiline text inside box with smart anchor."""
     lines = re.findall(r"(.{1,%d})(?:\s|$)" % line_width, text)
+    if len(lines) > 4 and font_size > 10:
+        return draw_text(ctx, text, box, anchor, fill, font_size - 10, line_width + 2)
 
     # https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html#text-anchors
     if anchor[0] == "l":
@@ -42,15 +45,17 @@ def draw_text(
     if anchor[1] == "t":
         y = box[1]
     elif anchor[1] == "m":
-        y = box[1] + (box[3] - len(lines) * font.size) // 2
+        y = box[1] + (box[3] - len(lines) * font_size) // 2
     elif anchor[1] == "b":
-        y = box[1] + (box[3] - len(lines) * font.size)
+        y = box[1] + (box[3] - len(lines) * font_size)
     else:
         raise NotImplementedError(anchor)
 
+    font = ImageFont.truetype(font_path(), font_size, encoding="UTF-8")
+
     for line in lines:
         ctx.text((x, y), line, anchor=align, fill=fill, font=font)
-        y += font.size
+        y += font_size
 
 
 def draw_cover(title: str, artist: str, cover: bytes) -> bytes:
@@ -58,16 +63,13 @@ def draw_cover(title: str, artist: str, cover: bytes) -> bytes:
     assert cover_canvas.size == (400, 400)
 
     canvas = Image.new("RGB", (WIDTH, HEIGHT))
-    canvas.paste(cover_canvas, (WIDTH - 450, (HEIGHT - 400) // 2))
+    canvas.paste(cover_canvas, (WIDTH // 2 - 200, 30))
 
     ctx = ImageDraw.Draw(canvas)
-    font = ImageFont.truetype(font_path(), 60, encoding="UTF-8")
     if title:
-        draw_text(ctx, title, (0, 0, WIDTH - 450, HEIGHT2 - 25), "mb", "white", font)
+        draw_text(ctx, title, (0, HEIGHT2, WIDTH, HEIGHT4), "mb", "white", 60, 35)
     if artist:
-        draw_text(
-            ctx, artist, (0, HEIGHT2 + 25, WIDTH - 450, HEIGHT2), "mt", "grey", font
-        )
+        draw_text(ctx, artist, (0, HEIGHT4 * 3, WIDTH, HEIGHT4), "mt", "grey", 50, 40)
 
     bytes = io.BytesIO()
     canvas.save(bytes, format="JPEG", quality=75)
@@ -78,11 +80,10 @@ def draw_lyrics(first: str | None, second: str | None) -> bytes:
     canvas = Image.new("RGB", (WIDTH, HEIGHT))
 
     ctx = ImageDraw.Draw(canvas)
-    font = ImageFont.truetype(font_path(), 100, encoding="UTF-8")
     if first:
-        draw_text(ctx, first, (0, 50, WIDTH, HEIGHT2 - 50), "mm", "white", font)
+        draw_text(ctx, first, (0, 50, WIDTH, HEIGHT2 - 50), "mm", "white", 100)
     if second:
-        draw_text(ctx, second, (0, HEIGHT2, WIDTH, HEIGHT2 - 50), "mm", "grey", font)
+        draw_text(ctx, second, (0, HEIGHT2, WIDTH, HEIGHT2 - 50), "mm", "grey", 100)
 
     bytes = io.BytesIO()
     canvas.save(bytes, format="JPEG", quality=75)
